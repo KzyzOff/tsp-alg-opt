0ce37075d39828b89f86bd087636299e97228ffe
KzyzOff <krzysztof.dybus@o2.pl>
2024-10-15 17:05:32 +0200

    added vec2str util function

--- include/utils.hpp
+++ include/utils.hpp
@@ -1,3 +1,8 @@
 #pragma once
 
+#include <string>
+#include <vector>
+
 float calc_distance(float x1, float y1, float x2, float y2);
+
+std::string vec2str(const std::vector<int>& vec);
--- src/utils.cpp
+++ src/utils.cpp
@@ -1,10 +1,20 @@
 #include "utils.hpp"
 
 #include <cmath>
+#include <sstream>
 
 float calc_distance(float x1, float y1, float x2, float y2) {
 	float dx = std::abs(x1 - x2);
 	float dy = std::abs(y1 - y2);
 
 	return std::sqrt(dx * dx + dy * dy);
+}
+
+std::string vec2str(const std::vector<int>& vec) {
+	std::ostringstream result;
+	for ( auto el : vec ) {
+		result << el;
+	}
+
+	return result.str();
 }
